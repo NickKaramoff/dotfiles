369bcb269d1ec3859c99ab84ca6060feeab60ee7
Nikita Karamov <nick@karamoff.dev>
2021-02-13 12:52:59 +0100

    feat(zsh): add .local and cargo to PATH

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -68,11 +68,15 @@ export PATH="/usr/local/opt/gnu-getopt/bin:$PATH"
 
 export PATH="/usr/local/opt/ncurses/bin:$PATH" # fixes nvm
 
-export PATH="$HOME/.poetry/bin:$PATH"
-
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
 export PATH="/usr/local/sbin:$PATH"
+
+export PATH="/Users/nick/.local/bin:$PATH"
+
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# export PATH="$HOME/.poetry/bin:$PATH"
 
 
 
